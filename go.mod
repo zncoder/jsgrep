3a704ba29fa2eafe259f16015f014033baaec2bd
Bin Zhang <bin.zhang@databricks.com>
2024-11-22 09:32:54 -0800

    grep json by key and value regexp

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/zncoder/jsgrep
+
+go 1.21.13
+
+require (
+	github.com/zncoder/check v0.0.0-20240921154110-f068e8bec397
+	github.com/zncoder/mygo v0.0.0-20240923043316-338d88ff1fe8
+)
---
+++ jsgrep.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/zncoder/check"
+)
+
+func loadJSON() any {
+	var js any
+	dec := json.NewDecoder(os.Stdin)
+	dec.UseNumber()
+	check.E(dec.Decode(&js)).F("decode json from stdin")
+	return js
+}
+
+type jsonKey struct {
+	Key   string
+	Index int
+}
+
+func (jk jsonKey) String() string {
+	if jk.Key != "" {
+		return jk.Key
+	} else {
+		return fmt.Sprintf("[%d]", jk.Index)
+	}
+}
+
+type jsonEntry struct {
+	Key   string
+	Value any
+}
+
+func toJqKey(keyPrefix []jsonKey) string {
+	var sb strings.Builder
+	for _, k := range keyPrefix {
+		sb.WriteString(".")
+		sb.WriteString(k.String())
+	}
+	return sb.String()
+}
+
+func (je jsonEntry) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]any{je.Key: je.Value})
+}
+
+func walkObjectTree(keyPrefix []jsonKey, key string, val any, match func(string, any) bool) []jsonEntry {
+	var matched []jsonEntry
+	switch v := val.(type) {
+	case map[string]any:
+		for k, sv := range v {
+			submatched := walkObjectTree(append(keyPrefix, jsonKey{Key: k}), k, sv, match)
+			if len(submatched) > 0 {
+				matched = append(matched, submatched...)
+			}
+		}
+	case []any:
+		for i, sv := range v {
+			submatched := walkObjectTree(append(keyPrefix, jsonKey{Index: i}), key, sv, match)
+			if len(submatched) > 0 {
+				matched = append(matched, submatched...)
+			}
+		}
+	case string, json.Number, bool, nil:
+		if match(key, v) {
+			matched = append(matched, jsonEntry{Key: toJqKey(keyPrefix), Value: v})
+		}
+	}
+	return matched
+}
+
+func matchValue(keyRe, valRe *regexp.Regexp, key string, val any) bool {
+	if key != "" && !keyRe.MatchString(key) {
+		return false
+	}
+
+	switch v := val.(type) {
+	case string:
+		if valRe.MatchString(v) {
+			return true
+		}
+	case json.Number:
+		if valRe.MatchString(v.String()) {
+			return true
+		}
+	case bool:
+		if valRe.MatchString(fmt.Sprintf("%v", v)) {
+			return true
+		}
+	case nil:
+		if valRe.MatchString("null") {
+			return true
+		}
+	}
+	return false
+}
+
+func matchJSON(keyRe, valRe *regexp.Regexp, indent bool) {
+	js := loadJSON()
+	matched := walkObjectTree(nil, "", js, func(key string, v any) bool { return matchValue(keyRe, valRe, key, v) })
+	enc := json.NewEncoder(os.Stdout)
+	if indent {
+		enc.SetIndent("", "    ")
+	}
+	check.E(enc.Encode(matched)).F("encode json to stdout")
+}
+
+func main() {
+	indent := flag.Bool("i", false, "indent")
+	flag.Parse()
+	check.T(flag.NArg() == 2).F("usage: key_re value_re")
+	keyRe := regexp.MustCompile(flag.Arg(0))
+	valRe := regexp.MustCompile(flag.Arg(1))
+
+	matchJSON(keyRe, valRe, *indent)
+}
